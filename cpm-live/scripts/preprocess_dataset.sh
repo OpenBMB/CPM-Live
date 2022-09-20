d875a1fc8ce20747cdaf7ca2541c2a15e0ebc3bf
zh-zheng <qishuangyiren@outlook.com>
2022-09-20 11:11:13 +0800

    add data sample

--- cpm-live/scripts/preprocess_dataset.sh
+++ cpm-live/scripts/preprocess_dataset.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CMD="python preprocess_dataset.py --input your/path/to/dataset --output_path path/to/binary/file --output_name cpm_ant_data"
+CMD="python preprocess_dataset.py --input data_sample.txt --output_path path/to/binary/file --output_name cpm_ant_data"
 
 echo $CMD
 $CMD
