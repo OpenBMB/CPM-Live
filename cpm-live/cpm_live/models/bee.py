ac2cb6cc7f4e6872b2cc6bf452303af8c8366117
zengguoyang <qbjooo@qq.com>
2022-09-21 18:17:11 +0800

    FX: typing

--- cpm-live/cpm_live/models/bee.py
+++ cpm-live/cpm_live/models/bee.py
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional, Tuple, TypedDict
+from typing import List, Optional, Tuple
+from typing_extensions import TypedDict
 import torch
 
-from ..tokenizers.bee import CPMBeeTokenizer
 from ..utils import Config
 from ..layers import Encoder, EmbeddingExt, BucketPositionBias
 import bmtrain as bmt
