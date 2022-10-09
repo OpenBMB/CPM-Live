5d52d718f6e07272486855e4ba3900c3babf82fe
zengguoyang <qbjooo@qq.com>
2022-10-09 13:53:19 +0800

    add setup.py

---
+++ cpm-live/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cpm_live",
+    version="0.1.0",
+    author="OpenBMB",
+    author_email="openbmb@gmail.com",
+    description="Toolkit for CPM-Live",
+    packages=find_packages(include=("cpm_live",)),
+    install_requires=[
+        "numpy",
+        "torch>=1.10",
+        "bmtrain>=0.1.8",
+        "jieba",
+        "tqdm",
+        "tensorboard",
+        "numpy>=1.21.0",
+    ],
+)
