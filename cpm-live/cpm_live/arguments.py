fa10dc85d4884e04c6484318942400210ad57004
Jeffrey G <gongbaitao11@gmail.com>
2023-05-14 12:40:33 +0800

    Fix args use_delta and support bmt-0.2+ (#412)
    * Fix args  when set False
    
    * Fix global steps in tensorboard when finetuning
    
    * Fix early stop in finetune
    
    * support bmt-0.2.0+ in pretrain & finetune
    
    * Add eval loss in tensorboard
    
    * Modify save logic & early stop in finetune

--- cpm-live/cpm_live/arguments.py
+++ cpm-live/cpm_live/arguments.py
@@ -117,7 +117,12 @@ def add_finetune_args(parser: argparse.ArgumentParser):
     group = parser.add_argument_group("finetune", "fintune configurations")
     group.add_argument("--epoch", type=int, default=1, help="number of training epochs")
     group.add_argument("--task-name", type=str, default="task", help="name of training task")
-    group.add_argument("--use-delta", type=bool, default=True, help="use delta tuning or not")
+    group.add_argument(
+        "--use-delta",
+        action="store_true",
+        default=False,
+        help="use delta tuning or not"
+    )
     group.add_argument("--eval_dataset", type=str, help="path to eval dataset")
     group.add_argument(
         "--drop-last",
@@ -125,7 +130,7 @@ def add_finetune_args(parser: argparse.ArgumentParser):
         default=False,
         help="drop data from each epoch that cannot be formed into a complete batch at the end",
     )
-    group.add_argument("--test-interval", type=int, default=500, help="test interval")
+    group.add_argument("--eval-interval", type=int, default=500, help="eval interval")
     group.add_argument("--early-stop-patience", type=int, default=5, help="early stop steps")
     return parser
 
